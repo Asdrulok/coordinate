0428ea1d942f325e421e1ac6292f67238f74e5a4
Asdrulok <yeidenalberto5@gmail.com>
2026-01-08 07:29:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 
-app.use(express.json());
-
-app.post('/', (req, res) => {
-    const { lat, lon } = req.body;
-    console.log(`Coordenadas recibidas: Latitud ${lat}, Longitud ${lon}`);
-    res.send({ message: 'Coordenadas recibidas' });
-});
-
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
-});
